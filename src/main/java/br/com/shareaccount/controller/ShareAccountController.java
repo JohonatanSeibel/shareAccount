2ee452deab05dc0c0c9dfa511c9e85e26351f65b
Johonatan Seibel <globalsys.johonatan@willbank.com.br>
2023-01-30 09:44:35 -0300

    Inplements DockFile anda update Method to request to POST

--- src/main/java/br/com/shareaccount/controller/ShareAccountController.java
+++ src/main/java/br/com/shareaccount/controller/ShareAccountController.java
@@ -12,10 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -26,7 +23,7 @@ public class ShareAccountController {
 
     private final ShareAccountService shareAccountService;
     @ResponseStatus(code = HttpStatus.OK)
-    @GetMapping(path = "/shareAccount", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "shareAccount", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Returns Share Account With Friends")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Account successfully listed."),
             @ApiResponse(code = 400, message = "Bad Request"),
